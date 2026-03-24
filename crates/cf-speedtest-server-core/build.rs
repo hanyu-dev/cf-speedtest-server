737cfb169d6ccba6f85c5c7129552977a55533cf
Hantong Chen <cxwdyx620@gmail.com>
2026-03-25 01:51:24 +0800

    refactor(cd-speedtest-core): update build.rs

--- crates/cf-speedtest-server-core/build.rs
+++ crates/cf-speedtest-server-core/build.rs
@@ -1,39 +1,46 @@
 //! Get git infos
 
-use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::process::Command;
+use std::{env, io};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=../../crates");
 
-    let main_version = env!("CARGO_PKG_VERSION");
-    let branch = Command::new("git")
-        .args(["branch", "--show-current"])
-        .output()
-        .map(|o| String::from_utf8(o.stdout).unwrap())
-        .unwrap();
-    let commit = Command::new("git")
-        .args(["describe", "--always"])
-        .output()
-        .map(|o| String::from_utf8(o.stdout).unwrap())
-        .unwrap();
-    let release_mode = if cfg!(debug_assertions) || cfg!(test) {
-        "DEBUG"
+    let version = env!("CARGO_PKG_VERSION");
+
+    let source =
+        env::var("CF_SPEEDTEST_SERVER_CORE_BUILD_SOURCE").unwrap_or_else(|_| "commit".to_string());
+
+    let commit = env::var("CF_SPEEDTEST_SERVER_CORE_BUILD_COMMIT")
+        .or_else(|_| run("git", &["describe", "--always"]))
+        .unwrap_or_else(|_| "unknown".to_string());
+
+    let build = if cfg!(debug_assertions) || cfg!(test) {
+        "debug"
     } else {
-        "RELEASE"
+        "release"
     };
 
-    let version =
-        format!("{}-{}-{}-{}", main_version, branch, commit, release_mode).replace('\n', "");
+    let version = format!("{version}+{source}.{commit}.{build}").replace('\n', "");
+
     File::create(Path::new(&env::var("OUT_DIR")?).join("VERSION"))?
         .write_all(version.trim().as_bytes())?;
 
-    // let now = chrono::Local::now().to_rfc3339();
-    // File::create(Path::new(&env::var("OUT_DIR")?).join("BUILD_TIME"))?
-    //     .write_all(now.trim().as_bytes())?;
-
     Ok(())
+}
+
+fn run(cmd: &str, args: &[&str]) -> io::Result<String> {
+    let output = Command::new(cmd).args(args).output()?;
+
+    if output.status.success() {
+        String::from_utf8(output.stdout).map_err(io::Error::other)
+    } else {
+        Err(io::Error::other(format!(
+            "Command `{cmd} {args:?}` failed with error: {}",
+            String::from_utf8_lossy(&output.stderr)
+        )))
+    }
 }
